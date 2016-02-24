77509404fe1ce838e0e818fd93e62fbce3c19c50
sunnyheng <1171967202@qq.com>
2016-02-24 14:58:43 +0800

    上传功能已完成

--- aliupload_yq.py
+++ aliupload_yq.py
@@ -4,7 +4,7 @@ import random
 import time 
 import logging
 
-import logging.handlers  
+import logging
 from oas.oas_api import OASAPI
 from oas.ease.api import APIProxy
 from oas.ease.exceptions import *
@@ -16,34 +16,51 @@ from oas.ease.job import *
 
 import os
 
-LOG_FILE="test_yq.log"
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes = 1024*1024, backupCount = 5)
-fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'  
-formatter = logging.Formatter(fmt)
-handler.setFormatter(formatter)
-log.addHandler(handler)
-log.setLevel(logging.DEBUG)
+logging.basicConfig(filename = 'test_yq.log',level = logging.DEBUG,filemode = 'a', format = '%(asctime)s -%(name)s %(lineno)d %(funcName)s: %(message)s')
+# LOG_FILE="test_yq.log"
+# handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes = 1024*1024, backupCount = 5)
+# fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'  
+# formatter = logging.Formatter(fmt)
+# handler.setFormatter(formatter)
+# log.addHandler(handler)
+# log.setLevel(logging.DEBUG)
 
 api = OASAPI('cn-hangzhou.oas.aliyuncs.com', 'eygUsBeE9HAp0ltu', 'v5oQ7ZmxaDpSaWLzfHJLermnGSFpDI')
-
 vault = Vault.create_vault(api, 'szyq')
 
-path='e:/banben/upload/1.xlsx'
+path='e:/banben/upload/upload/'
+
+archive_id_list=[]
+filename=['a.txt','b.txt','c.txt','d.txt']
+
+#localpath="e:/banben/upload/2.xlsx"
+
+def get_archive_id(archive_id):
+    archive_id_list.append(archive_id)
+    return archive_id_list
+
 
 def upload():
-    archive_id = vault.upload_archive(path)
-    return archive_id
+    for name in filename:
+        print 'start upload '+name
+        logging.info(name+":start")
+        archive_id = vault.upload_archive(path+name)
+        get_archive_id(archive_id)
+        print archive_id
+        logging.info('get:'+archive_id)
+    print 'uploading success!'
+    logging.info('uploading success!')
 
 
-def delete():
-    archive_id = upload()
-    vault.delete_archive(archive_id)
-    print archive_id,'has deleted!'
+# def delete():
+#     archive_id = upload()
+#     vault.delete_archive(archive_id)
+#     print archive_id,'has deleted!'
 
-def download():
-    archive_id = upload(path)
-    job = vault.retrieve_archive(archive_id)
-    job.download_to_file(localpath)
+# def download():
+#     archive_id = upload()
+#     job = vault.retrieve_archive(archive_id)
+#     job.download_to_file(localpath)
 
 if __name__ == '__main__':
     upload()
